273f55262cacd78505fdd0fc606988a1e1fbd0e1
Prakarsh20 <prakarshkumar9204@gmail.com>
2026-08-28 07:34:38 +0530

    quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -14,6 +14,37 @@ Checks:
     - Consistency between related fields
 ===============================================================================
 */
+-- FOR CUST_INFO
+-- Check For Nulls or Duplicates in Primary Key
+-- Expectation: No Result
+SELECT
+    cst_id,
+    COUNT(*)
+FROM silver.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL;
+
+-- Check for unwanted Spaces
+-- Expectation: No Results
+SELECT cst_firstname
+FROM silver.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname);
+---------
+SELECT cst_lastname
+FROM silver.crm_cust_info
+WHERE cst_lastname != TRIM(cst_lastname);
+
+
+
+-- Data Standardization & Consistency
+SELECT DISTINCT cst_marital_status
+FROM silver.crm_cust_info;
+--------
+SELECT DISTINCT cst_gndr
+FROM silver.crm_cust_info;
+
+SELECT * FROM silver.crm_cust_info;
+-----------------------------------------------------------------
 -- check for nulls and duplicates in primary key
 -- Expectation: No Result
 select cst_id, count(*) from bronze.crm_cust_info 
